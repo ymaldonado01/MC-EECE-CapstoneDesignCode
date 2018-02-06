ebdd3405a6d4c853a9a6411b5d668ca7045227cf
Yobely <ymaldonado01@manhattan.edu>
2018-02-06 17:52:39 -0500

    App Code
    This is the Android Studio Project for our UGarden App

---
+++ UGarden/app/src/main/java/com/example/mc_capstone/ugarden/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.mc_capstone.ugarden;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Thread t = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(1000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                TextView tdate = (TextView) findViewById(R.id.date);
+                                long date = System.currentTimeMillis();
+                                SimpleDateFormat sdf = new SimpleDateFormat("MMM dd yyyy\nhh:mm a");
+                                String dateString = sdf.format(date);
+                                tdate.setText(dateString);
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+        t.start();
+    }
+
+private static final String LOG_TAG=MainActivity.class.getSimpleName();
+    public void launchstatusdetails(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+    }
+}
